a6fe897ad21c3e8d3e7891fbafb7d47930d7a3aa
sala03 <sala03@targettrust.local>
2016-04-14 22:19:11 -0300

    Auala JUnit 1

--- aula-01-01/src/main/java/br/com/tt/Calculadora.java
+++ aula-01-01/src/main/java/br/com/tt/Calculadora.java
@@ -5,7 +5,7 @@ public class Calculadora {
 		return val1 + val2;
 	}
 	
-	public int dividir(int val1, int val2){
-		return val1 / val2;
+	public Double dividir(Double val1, Double val2){
+		return val1==null ? 0: val1 / (val2 ==null ? 0: val2);
 	}
 }
--- aula-01-01/src/test/java/br/com/tt/CalculadoraTest.java
+++ aula-01-01/src/test/java/br/com/tt/CalculadoraTest.java
@@ -11,21 +11,72 @@ public class CalculadoraTest {
 	public void testSomar() {
 		int val1 = 2;
 		int val2 = 2;
-		
+
 		Calculadora calculadora = new Calculadora();
 		int soma = calculadora.somar(val1, val2);
-		Assert.assertEquals(4, soma);	
-		
+		Assert.assertEquals(4, soma);
+	}
+
+	@Test
+	public void testDividirInteirosIguais() {
+		Double val1 = 2D;
+		Double val2 = 2D;
+
+		Calculadora calculadora = new Calculadora();
+		Double dividir = calculadora.dividir(val1, val2);
+		Assert.assertEquals(new Double(1), new Double(dividir));
+	}
+
+	@Test
+	public void testDividirPrimeiroValorMenor() {
+		Double val1 = 1D;
+		Double val2 = 4D;
+
+		Calculadora calculadora = new Calculadora();
+		assertEquals(new Double(0.25), new Double(calculadora.dividir(val1, val2)));
+	}
+
+	@Test
+	public void testDividirSegundoMenor() {
+
+		Calculadora calculadora = new Calculadora();
+		assertEquals(new Double(2.5), new Double(calculadora.dividir(5D, 2D)));
 	}
 	
 	@Test
-	public void testDividir() {
-		int val1 = 4;
-		int val2 = 2;
+	public void testDividirSegundoZero() {
 		
-		Calculadora calculadora = new Calculadora();
-		int dividir = calculadora.dividir(val1, val2);
-		Assert.assertEquals(2, dividir);
+		assertTrue(new Calculadora().dividir(5D, 0D).isInfinite());
 	}
+	
+	@Test
+	public void testDividirPrimeiroZero() {
+		
+		assertEquals(new Double(0), new Calculadora().dividir(0D, 5D));
+	}
+	
+	
+	@Test
+	public void testDividirPrimeiroNull() {
+		
+		assertEquals(new Double(0), new Calculadora().dividir(null, 5D));
+	}
+	
+	@Test
+	public void testDividirSegundoNull() {
+		
+		assertTrue(new Calculadora().dividir(5D, null).isInfinite());
+	}
+	
 
+	@Test
+	public void testDividirDoisNull() {	
+		assertEquals(new Double(0), new Calculadora().dividir(null, null));
+	}
+	
+	@Test (expected = NumberFormatException.class)
+	public void testDividirString() {	
+		assertEquals(new Double(0), new Calculadora().dividir(new Double("AAA"), null));
+	}
+	
 }
