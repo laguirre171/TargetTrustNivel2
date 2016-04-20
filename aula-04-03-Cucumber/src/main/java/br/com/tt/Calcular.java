a76b2e37d7aa3d38e133b8a19a4b9cef8d312061
sala03 <sala03@targettrust.local>
2016-04-19 21:41:54 -0300

    Quarta Aula1

--- aula-04-03-Cucumber/src/main/java/br/com/tt/Calcular.java
+++ aula-04-03-Cucumber/src/main/java/br/com/tt/Calcular.java
@@ -1,29 +1,27 @@
 package br.com.tt;
 
 public class Calcular {
-	
-	private Integer valor1;
-	private Integer valor2;
-	private Integer resultado;
-	
-	public void setValor1(Integer v1) {
-		this.valor1 = v1;
-		
-	}
 
-	public void setValor2(Integer v2) {
-		this.valor2 = v2;
-		
-	}
+  private Integer valor1;
+  private Integer valor2;
+  private Integer resultado;
 
-	public void somar() {
-		resultado = valor1 + valor2;		
-	}
+  public void setValor1(Integer v1) {
+    this.valor1 = v1;    
+  }
 
-	public Integer getResultado() {
-		return resultado;
-	}
-	
-	
+  public void setValor2(Integer v2) {
+    this.valor2 = v2;
+
+  }
+
+  public void somar() {
+    resultado = valor1 + valor2;
+  }
+
+  public Integer getResultado() {
+    return resultado;
+  }
+
 
 }
